6a028f4ae1f6a109c87bab534e8a42e6b6858a63
mituttta051 <amityutneva@mail.ru>
2025-02-04 12:31:38 +0300

    Initial commit

---
+++ src/test/java/cybercooker/recipeservice/RecipeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cybercooker.recipeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
