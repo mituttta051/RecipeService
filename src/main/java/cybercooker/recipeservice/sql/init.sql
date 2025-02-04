a1cadf878a598223a93efd63b100a11236db9067
mituttta051 <amityutneva@mail.ru>
2025-02-04 17:52:40 +0300

    feat: added compose.yaml for postgres database. fix: creating tables only if they do not exist

--- src/main/java/cybercooker/recipeservice/sql/init.sql
+++ src/main/java/cybercooker/recipeservice/sql/init.sql
@@ -4,31 +4,31 @@ CREATE DATABASE recipe_service;
 \c recipe_service;
 
 -- Create the recipes table
-CREATE TABLE recipe (
+CREATE TABLE IF NOT EXISTS recipe (
     id INT NOT NULL,
-    spaceID INT NOT NULL,
+    space_id INT NOT NULL,
     name VARCHAR(255) NOT NULL,
     ingredients INT[] NOT NULL,
     description TEXT NOT NULL,
     servings_number INT NOT NULL,
     cook_time INT NOT NULL,
     tags JSON,
-    PRIMARY KEY (id, spaceID)  
+    PRIMARY KEY (id, space_id)  
 );
 
 -- Create the ingredients table
-CREATE TABLE ingredient (
+CREATE TABLE IF NOT EXISTS ingredient (
     id INT NOT NULL,
-    spaceID INT NOT NULL,
+    space_id INT NOT NULL,
     name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id, spaceID)
+    PRIMARY KEY (id, space_id)
 );
 
 -- Create the tags table
-CREATE TABLE tag (
+CREATE TABLE IF NOT EXISTS tag (
     id INT NOT NULL,
-    spaceID INT NOT NULL,
+    space_id INT NOT NULL,
     name VARCHAR(255) NOT NULL,
     values TEXT[] NOT NULL,
-    PRIMARY KEY (id, spaceID)
+    PRIMARY KEY (id, space_id)
 ); 
